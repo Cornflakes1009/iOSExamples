e863e35955282be90f20b34fe332b14c719f33c8
Harold Davidson <harold.davidson89@gmail.com>
2024-12-04 18:46:07 -0800

    Create TikTok Clone project

---
+++ TikTokClone/TikTokClone/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Harold Davidson on 12/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
