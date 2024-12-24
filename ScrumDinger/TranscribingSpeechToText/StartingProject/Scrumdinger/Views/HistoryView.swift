c7a57ed1e9a694f081b8186552247babe3afddd6
Harold Davidson <harold.davidson89@gmail.com>
2024-12-23 22:32:45 -0800

    Complete Scrumdinger

---
+++ ScrumDinger/TranscribingSpeechToText/StartingProject/Scrumdinger/Views/HistoryView.swift
@@ -0,0 +1,42 @@
+//
+//  HistoryView.swift
+//  Scrumdinger
+//
+//  Created by Harold Davidson on 12/23/24.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    let history: History
+    
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                Divider()
+                    .padding(.bottom)
+                Text("Attendees")
+                    .font(.headline)
+                Text(history.attendeeString)
+                if let transcript = history.transcript {
+                    Text("Transcript")
+                        .font(.headline)
+                        .padding(.top)
+                    Text(transcript)
+                }
+            }
+        }
+        .navigationTitle(Text(history.date, style: .date))
+        .padding()
+    }
+}
+
+extension History {
+    var attendeeString: String {
+        ListFormatter.localizedString(byJoining: attendees.map { $0.name })
+    }
+}
+
+//#Preview {
+//    HistoryView()
+//}
