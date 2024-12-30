045b92d70e1d4fdd26e1d71396f52e81e209e385
Harold Davidson <harold.davidson89@gmail.com>
2024-12-29 22:35:00 -0800

    Complete Swiftful thinking initializers and enums

--- Swiftful Thinking SwiftUI/SwiftfulThinking/SwiftfulThinking/Views/Initializers.swift
+++ Swiftful Thinking SwiftUI/SwiftfulThinking/SwiftfulThinking/Views/Initializers.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct Initializers: View {
+    let backgroundColor: Color
+    let count: Int
+    let title: String
+    
+    init(count: Int, fruit: Fruit) {
+        self.count = count
+        
+        switch fruit {
+        case .apple:
+            self.title = "Apples"
+            self.backgroundColor = .red
+        case .orange:
+            self.title = "Oranges"
+            self.backgroundColor = .orange
+        }
+    }
+    
+    enum Fruit {
+        case apple
+        case orange
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 12) {
+            Text("\(count)")
+                .font(.largeTitle)
+                .foregroundStyle(.white)
+                .underline()
+            Text("\(title)")
+                .font(.headline)
+                .foregroundStyle(.white)
+        }
+        .frame(width: 150, height: 150)
+        .background(backgroundColor)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
 
 #Preview {
-    Initializers()
+    HStack {
+        Initializers(count: 25, fruit: .apple)
+        Initializers(count: 17, fruit: .orange)
+    }
+    
 }
