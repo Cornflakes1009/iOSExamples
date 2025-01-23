cf2c14ebe27bca73f036d03554c785f63dc53467
Harold Davidson <harold.davidson89@gmail.com>
2025-01-22 22:21:43 -0800

    Complete search in Crypto App

--- Projects/CryptoApp/CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
+++ Projects/CryptoApp/CryptoApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -23,10 +23,27 @@ class HomeViewModel: ObservableObject { // observable object is so that it can b
     }
     
     func addSubscribers() {
-        dataService.$allCoins
+        
+        // this updates allCoins
+        $searchText
+            .combineLatest(dataService.$allCoins) // if either the search text or allcoins gets changed, this calls
+            .debounce(for: .seconds(0.5), scheduler: DispatchQueue.main) // this waits a half second before running the code below. 
+            .map(filterCoins)
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
+        guard !text.isEmpty else { return coins } // there's no search text entered
+        
+        let lowercasedText = text.lowercased()
+        
+        return coins.filter { (coin) in // trying to find if the text entered matches the name, symbol, or id of any coins
+            return coin.name.lowercased().contains(lowercasedText) ||
+            coin.symbol.lowercased().contains(lowercasedText) ||
+            coin.id.contains(lowercasedText)
+        }
     }
 }
