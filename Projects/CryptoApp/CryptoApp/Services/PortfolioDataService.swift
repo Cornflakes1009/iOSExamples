d75a4e98ad98b7f4859c1f30277441060de1eb39
Harold Davidson <harold.davidson89@gmail.com>
2025-02-20 22:24:17 -0800

    Complete PortfolioDataService

--- Projects/CryptoApp/CryptoApp/Services/PortfolioDataService.swift
+++ Projects/CryptoApp/CryptoApp/Services/PortfolioDataService.swift
@@ -6,7 +6,29 @@
 //
 
 import Foundation
+import CoreData
 
 class PortfolioDataService {
+    private let container: NSPersistentContainer
+    private let containerName: String = "PortfolioContainer"
+    private let entity: String = "PortfolioEntity" // this comes from the entity name in CoreData. Look at PortfolioContainer in Models
+    @Published var savedEntities: [PortfolioEntitiy] = []
     
+    init() {
+        container = NSPersistentContainer(name: containerName)
+        container.loadPersistentStores { _, error in
+            if let error = error {
+                print("Error loading Core Data!", error)
+            }
+        }
+    }
+    
+    private func getPortfolio() {
+        let request = NSFetchRequest<PortfolioEntitiy>(entityName: entity)
+        do {
+            savedEntities = try container.viewContext.fetch(request)
+        } catch {
+            print("Error fetching Portfolio Entities:", error)
+        }
+    }
 }
