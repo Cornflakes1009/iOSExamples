ef156c927e6d6264093a83e2646e65e4a075a14f
Harold Davidson <harold.davidson89@gmail.com>
2025-02-21 23:53:39 -0800

    Complete the PortfolioDataService

--- Projects/CryptoApp/CryptoApp/Services/PortfolioDataService.swift
+++ Projects/CryptoApp/CryptoApp/Services/PortfolioDataService.swift
@@ -12,6 +12,7 @@ class PortfolioDataService {
     private let container: NSPersistentContainer
     private let containerName: String = "PortfolioContainer"
     private let entity: String = "PortfolioEntity" // this comes from the entity name in CoreData. Look at PortfolioContainer in Models
+    
     @Published var savedEntities: [PortfolioEntitiy] = []
     
     init() {
@@ -20,8 +21,25 @@ class PortfolioDataService {
             if let error = error {
                 print("Error loading Core Data!", error)
             }
+            self.getPortfolio()
         }
     }
+    
+    // MARK: - PUBLIC
+    func updatePortfolio(coin: CoinModel, amount: Double) {
+        // check if coin is already in portfolio
+        if let entity = savedEntities.first(where: { $0.coinID == coin.id }) {
+            if amount > 0 {
+                update(entity: entity, amount: amount)
+            } else {
+                delete(entity: entity)
+            }
+        } else {
+            add(coin: coin, amount: amount)
+        }
+    }
+    
+    // MARK: - PRIVATE
     
     private func getPortfolio() {
         let request = NSFetchRequest<PortfolioEntitiy>(entityName: entity)
@@ -30,5 +48,35 @@ class PortfolioDataService {
         } catch {
             print("Error fetching Portfolio Entities:", error)
         }
+    }
+    
+    private func add(coin: CoinModel, amount: Double) {
+        let entity = PortfolioEntitiy(context: container.viewContext)
+        entity.coinID = coin.id
+        entity.amount = amount
+        applyChanges()
+    }
+    
+    private func update(entity: PortfolioEntitiy, amount: Double) {
+        entity.amount = amount
+        applyChanges()
+    }
+    
+    private func delete(entity: PortfolioEntitiy) {
+        container.viewContext.delete(entity)
+        applyChanges()
+    }
+    
+    private func save() {
+        do {
+            try container.viewContext.save()
+        } catch let error {
+            print("Error saving to Core Data:", error)
+        }
+    }
+    
+    private func applyChanges() {
+        save()
+        getPortfolio()
     }
 }
