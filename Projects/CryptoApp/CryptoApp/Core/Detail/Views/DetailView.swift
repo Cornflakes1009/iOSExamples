c7435d10cfcaea4db41f3a41e3ea59d035a18ea2
Harold Davidson <harold.davidson89@gmail.com>
2025-03-02 22:36:46 -0800

    WIP

--- Projects/CryptoApp/CryptoApp/Core/Detail/Views/DetailView.swift
+++ Projects/CryptoApp/CryptoApp/Core/Detail/Views/DetailView.swift
@@ -8,11 +8,18 @@
 import SwiftUI
 
 struct DetailView: View {
+    
+    let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(coin.name)
     }
 }
 
 #Preview {
-    DetailView()
+    DetailView(coin: PreviewData.coin)
 }
