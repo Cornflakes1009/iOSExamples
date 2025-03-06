358e8362cff5fb874f1cddf0b0b9b61e60f45ab1
Harold Davidson <harold.davidson89@gmail.com>
2025-03-05 23:02:09 -0800

    Complete custom navigation to detail view on Crypto App

--- Projects/CryptoApp/CryptoApp/Core/Detail/Views/DetailView.swift
+++ Projects/CryptoApp/CryptoApp/Core/Detail/Views/DetailView.swift
@@ -7,19 +7,35 @@
 
 import SwiftUI
 
-struct DetailView: View {
+struct DetailLoadingView: View {
     
     @Binding var coin: CoinModel?
     
-    init(coin: Binding<CoinModel?>) {
-        self._coin = coin
+    var body: some View {
+        ZStack {
+            if let coin = coin {
+                DetailView(coin: coin)
+                
+            }
+        }
+    }
+}
+
+struct DetailView: View {
+    
+    let coin: CoinModel
+    
+    init(coin: CoinModel) {
+        self.coin = coin
     }
     
     var body: some View {
-        Text(coin?.name ?? "")
+        ZStack {
+            Text(coin.name)
+        }
     }
 }
 
 #Preview {
-    DetailView(coin: .constant(PreviewData.coin))
+    DetailView(coin: PreviewData.coin)
 }
