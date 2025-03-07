628a72a6c6c8b70c2911a236edd256af8117172f
Harold Davidson <harold.davidson89@gmail.com>
2025-03-06 23:10:20 -0800

    Create DetailModel

---
+++ Projects/CryptoApp/CryptoApp/Core/Detail/ViewModels/DetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailViewModel.swift
+//  CryptoApp
+//
+//  Created by Harold Davidson on 3/6/25.
+//
+
+import Foundation
---
+++ Projects/CryptoApp/CryptoApp/Extensions/String.swift
@@ -0,0 +1,17 @@
+//
+//  String.swift
+//  CryptoApp
+//
+//  Created by Harold Davidson on 3/6/25.
+//
+
+import Foundation
+
+extension String {
+    
+    
+    var removingHTMLOccurances: String {
+        return self.replacingOccurrences(of: "<[^>]+>", with: "", options: .regularExpression, range: nil)
+    }
+    
+}
