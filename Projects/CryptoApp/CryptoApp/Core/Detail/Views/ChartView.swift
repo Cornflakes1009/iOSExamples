b1d64177533d8515796fed7cdbc38883872b873f
Harold Davidson <harold.davidson89@gmail.com>
2025-03-22 23:28:56 -0700

    WIP

--- Projects/CryptoApp/CryptoApp/Core/Detail/Views/ChartView.swift
+++ Projects/CryptoApp/CryptoApp/Core/Detail/Views/ChartView.swift
@@ -10,19 +10,34 @@ import SwiftUI
 struct ChartView: View {
     
     let data: [Double]
+    let maxY: Double
+    let minY: Double
     
     init(coin: CoinModel) {
         data = coin.sparklineIn7D?.price ?? []
+        maxY = data.max() ?? 0
+        minY = data.min() ?? 0
     }
     
     var body: some View {
-        Path { path in
-            for index in data.indices {
-                let xPosition = UIScreen.main.bounds.width / CGFloat(data.count) * CGFloat(index + 1)
-                
-                
-                
+        GeometryReader { geometry in
+            Path { path in
+                for index in data.indices {
+                    let xPosition = geometry.size.width / CGFloat(data.count) * CGFloat(index + 1)
+                    
+                    let yAxis = maxY - minY // calculating the middle point for the graph
+                    
+                    let yPosition = (data[index] - minY) / CGFloat(yAxis * geometry.size.height)
+                    
+                    if index == 0 {
+                        path.move(to: CGPoint(x: 0, y: 0))
+                    }
+                    path.addLine(to: CGPoint(x: xPosition, y: 0))
+                    
+                    
+                }
             }
+            .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
         }
     }
 }
