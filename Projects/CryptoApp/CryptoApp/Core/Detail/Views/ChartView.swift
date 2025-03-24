0eebdc9543f0dab76cb081469da0e1ab08f2cdbd
Harold Davidson <harold.davidson89@gmail.com>
2025-03-23 23:03:30 -0700

    WIP

--- Projects/CryptoApp/CryptoApp/Core/Detail/Views/ChartView.swift
+++ Projects/CryptoApp/CryptoApp/Core/Detail/Views/ChartView.swift
@@ -12,36 +12,50 @@ struct ChartView: View {
     let data: [Double]
     let maxY: Double
     let minY: Double
+    let lineColor: Color
     
     init(coin: CoinModel) {
         data = coin.sparklineIn7D?.price ?? []
         maxY = data.max() ?? 0
         minY = data.min() ?? 0
+        
+        let priceChange = (data.last ?? 0) - (data.first ?? 0)
+        lineColor = priceChange > 0 ? Color.theme.green : Color.theme.red
     }
     
     var body: some View {
-        GeometryReader { geometry in
-            Path { path in
-                for index in data.indices {
-                    let xPosition = geometry.size.width / CGFloat(data.count) * CGFloat(index + 1)
-                    
-                    let yAxis = maxY - minY // calculating the middle point for the graph
-                    
-                    let yPosition = (data[index] - minY) / CGFloat(yAxis * geometry.size.height)
-                    
-                    if index == 0 {
-                        path.move(to: CGPoint(x: 0, y: 0))
-                    }
-                    path.addLine(to: CGPoint(x: xPosition, y: 0))
-                    
-                    
-                }
-            }
-            .stroke(Color.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+        VStack {
+            chartView
+                .frame(height: 200)
         }
     }
 }
 
 #Preview {
     ChartView(coin: PreviewData.coin)
+}
+
+extension ChartView {
+    private var chartView: some View {
+        GeometryReader { geometry in
+            Path { path in
+                for index in data.indices {
+                    
+                    let xPosition = geometry.size.width / CGFloat(data.count) * CGFloat(index + 1)
+                    
+                    let yAxis = maxY - minY // calculating the middle point for the graph
+                    
+                    let yPosition = (1 - CGFloat((data[index] - minY) / yAxis)) *  geometry.size.height
+                    
+                    if index == 0 {
+                        path.move(to: CGPoint(x: xPosition, y: yPosition))
+                    }
+                    path.addLine(to: CGPoint(x: xPosition, y: yPosition))
+                    
+                    
+                }
+            }
+            .stroke(lineColor, style: StrokeStyle(lineWidth: 2, lineCap: .round, lineJoin: .round))
+        }
+    }
 }
