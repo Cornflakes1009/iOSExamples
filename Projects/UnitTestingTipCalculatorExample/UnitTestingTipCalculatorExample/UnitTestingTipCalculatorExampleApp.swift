4000b8531e4521c562a182ebb7984440a0ff2fcf
Harold Davidson <harold.davidson89@gmail.com>
2025-04-02 22:53:17 -0700

    Create new Unit Testing Project

---
+++ Projects/UnitTestingTipCalculatorExample/UnitTestingTipCalculatorExample/UnitTestingTipCalculatorExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestingTipCalculatorExampleApp.swift
+//  UnitTestingTipCalculatorExample
+//
+//  Created by Harold Davidson on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestingTipCalculatorExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
