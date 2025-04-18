d6605257dadc8e69825baf40068a51b25c64df85
Harold Davidson <harold.davidson89@gmail.com>
2025-04-17 20:27:38 -0700

    WIP

--- Projects/CryptoApp/CryptoApp/Core/Launch/Views/LaunchView.swift
+++ Projects/CryptoApp/CryptoApp/Core/Launch/Views/LaunchView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct LaunchView: View {
+    @State private var loadingText: String = "Loading your portfolio..."
+    @State private var showLoadingText: Bool = false
+    
     var body: some View {
         ZStack {
             Color.launch.background
@@ -16,6 +19,22 @@ struct LaunchView: View {
             Image("logo-transparent")
                 .resizable()
                 .frame(width: 100, height: 100)
+            
+            // this ZStack is offset even though it's over the top of the image. The image should be the absolute center of the screen so that it transitions from the LaunchScreen.storyboard to this file seamlessly.
+            ZStack {
+                if showLoadingText {
+                    Text(loadingText)
+                        .font(.headline)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(Color.launch.accent)
+                        .transition(AnyTransition.scale.animation(.easeIn))
+                }
+                
+            }
+            .offset(y: 70)
+        }
+        .onAppear {
+            showLoadingText.toggle()
         }
     }
 }
