ea13788d39c6e527bbf326185c41d4044a8cff0f
Harold Davidson <harold.davidson89@gmail.com>
2025-05-30 20:14:40 -0700

    WIP

--- HackingWithSwiftUI/BetterRest/BetterRest/ContentView.swift
+++ HackingWithSwiftUI/BetterRest/BetterRest/ContentView.swift
@@ -12,14 +12,27 @@ struct ContentView: View {
     @State private var wakeUp = Date.now
     
     var body: some View {
+        Text(Date.now, format: .dateTime.day().month().year()) // May 30, 2025
+        Text(Date.now.formatted(date: .long, time: .shortened)) // May 30, 2025 at &:31PM
         Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
         DatePicker("Please enter a date:", selection: $wakeUp, in: Date.now...)
             .labelsHidden() // this hides the label. A screen reader would still read the string value
     }
     
     func exampleDates() {
-        let tomorrow = Date.now.addingTimeInterval(86400)
-        let range = Date.now...tomorrow
+//        var components = DateComponents()
+//        components.hour = 8
+//        components.minute = 0
+//        let date = Calendar.current.date(from: components) ?? .now
+//        
+//        let now = Date.now
+//        let tomorrow = Date.now.addingTimeInterval(86400)
+//        let range = now...tomorrow
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
+        
     }
 }
 
