edcfa9d655c0f1852167432b72fa9bc63c4b9c4c
Harold Davidson <harold.davidson89@gmail.com>
2025-06-01 23:45:46 -0700

    WIP

--- HackingWithSwiftUI/BetterRest/BetterRest/ContentView.swift
+++ HackingWithSwiftUI/BetterRest/BetterRest/ContentView.swift
@@ -10,16 +10,44 @@ import SwiftUI
 struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
+    @State private var coffeeAmount = 1
     
     var body: some View {
-        Text(Date.now, format: .dateTime.day().month().year()) // May 30, 2025
-        Text(Date.now.formatted(date: .long, time: .shortened)) // May 30, 2025 at &:31PM
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-        DatePicker("Please enter a date:", selection: $wakeUp, in: Date.now...)
-            .labelsHidden() // this hides the label. A screen reader would still read the string value
+//        Text(Date.now, format: .dateTime.day().month().year()) // May 30, 2025
+//        Text(Date.now.formatted(date: .long, time: .shortened)) // May 30, 2025 at &:31PM
+//        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+//        DatePicker("Please enter a date:", selection: $wakeUp, in: Date.now...)
+//            .labelsHidden() // this hides the label. A screen reader would still read the string value
+        NavigationStack {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
+                
+                
+            }
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
+        }
+        
     }
     
-    func exampleDates() {
+    func calculateBedtime() {
+        
+    }
+//    func exampleDates() {
 //        var components = DateComponents()
 //        components.hour = 8
 //        components.minute = 0
@@ -29,11 +57,11 @@ struct ContentView: View {
 //        let tomorrow = Date.now.addingTimeInterval(86400)
 //        let range = now...tomorrow
         
-        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
-        let hour = components.hour ?? 0
-        let minute = components.minute ?? 0
-        
-    }
+//        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+//        let hour = components.hour ?? 0
+//        let minute = components.minute ?? 0
+//        
+//    }
 }
 
 #Preview {
