8da18d801b186933603d51a18dd816e927db4fb3
Harold Davidson <harold.davidson89@gmail.com>
2025-06-02 23:53:11 -0700

    WIP

--- HackingWithSwiftUI/BetterRest/BetterRest/ContentView.swift
+++ HackingWithSwiftUI/BetterRest/BetterRest/ContentView.swift
@@ -6,11 +6,15 @@
 //
 
 import SwiftUI
+import CoreML
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
     @State private var coffeeAmount = 1
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showAlert = false
     
     var body: some View {
 //        Text(Date.now, format: .dateTime.day().month().year()) // May 30, 2025
@@ -40,12 +44,33 @@ struct ContentView: View {
             .toolbar {
                 Button("Calculate", action: calculateBedtime)
             }
+            .alert(alertTitle, isPresented: $showAlert) {
+                Button("OK") { }
+            } message: {
+                Text(alertMessage)
+            }
         }
         
     }
     
     func calculateBedtime() {
-        
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60 // hour in seconds.
+            let minute = (components.minute ?? 0) * 60 // minutes in seconds.
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        showAlert = true
     }
 //    func exampleDates() {
 //        var components = DateComponents()
