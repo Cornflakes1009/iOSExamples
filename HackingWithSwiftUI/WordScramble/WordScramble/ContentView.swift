92f2496c762d5300048406864d10d8b9b34dca17
Harold Davidson <harold.davidson89@gmail.com>
2025-06-06 23:52:12 -0700

    WIP

--- HackingWithSwiftUI/WordScramble/WordScramble/ContentView.swift
+++ HackingWithSwiftUI/WordScramble/WordScramble/ContentView.swift
@@ -8,28 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
-    let people = ["Harold", "Ajay", "Tiffany", "Jesiel"]
+    //let people = ["Harold", "Ajay", "Tiffany", "Jesiel"]
     
     var body: some View {
-        List {
-            Section("Section 1") {
-                ForEach(0..<5) {
-                    Text("Dynamic row \($0)")
-                }
-            }
-            
-            Section("Section 2") {
-                Text("Section 2 row")
-            }
-        }
-        .listStyle(.grouped)
+//        List {
+//            Section("Section 1") {
+//                ForEach(0..<5) {
+//                    Text("Dynamic row \($0)")
+//                }
+//            }
+//            
+//            Section("Section 2") {
+//                Text("Section 2 row")
+//            }
+//        }
+//        .listStyle(.grouped)
+//        
+//        List(0..<5) {
+//            Text("Static row \($0)")
+//        }
+//        
+//        List(people, id: \.self) {
+//            Text($0)
+//        }
+    }
+    func testStrings() {
+        let input = "a b c"
+        let letters = input.components(separatedBy: " ") // [String]
+        let letter = letters.randomElement()
+        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
         
-        List(0..<5) {
-            Text("Static row \($0)")
-        }
-        
-        List(people, id: \.self) {
-            Text($0)
+        // checking for misspelled words
+        let word = "swift"
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        let allGood = misspelledRange.location == NSNotFound // checking for misspelling
+    }
+    func testBundles() {
+        if let fileURL = Bundle.main.url(forResource: "somefile", withExtension: "txt") {
+            if let fileContents = try? String(contentsOf: fileURL) {
+                
+            }
         }
     }
 }
