1b8fdfeb7b47a3f8883e545e1355a0bb910153b8
Harold Davidson <harold.davidson89@gmail.com>
2025-06-10 23:28:51 -0700

    WIP

--- HackingWithSwiftUI/WordScramble/WordScramble/ContentView.swift
+++ HackingWithSwiftUI/WordScramble/WordScramble/ContentView.swift
@@ -11,9 +11,15 @@ struct ContentView: View {
     @State private var usedWords = [String]()
     @State private var rootWord: String = ""
     @State private var newWord: String = ""
+    @State private var score: Int = 0
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showError: Bool = false
     
     var body: some View {
         NavigationStack {
+            Text("Score: \(score)")
             List {
                 Section {
                     TextField("Enter a word", text: $newWord)
@@ -32,19 +38,54 @@ struct ContentView: View {
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
             .onAppear(perform: startGame)
+            .toolbar {
+                Button("New Word", action: startGame)
+            }
+
+            .alert(errorTitle, isPresented: $showError) {
+                Button("OK") {} // don't actually have to add an OK button. SwiftUI will do it automatically.
+            } message: {
+                Text(errorMessage)
+            }
         }
     }
     
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
-        guard answer.count > 0 else { return }
+        guard answer.count > 2 else {
+            wordError(title: "Word too short!", message: "Use a longer word!")
+            return
+        }
+        
+        guard answer != rootWord else {
+            wordError(title: "Can't do that!", message: "You have to choose a different word!")
+            return
+        }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already!", message: "Be more original!")
+            return
+        }
+        
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+        
+        guard isReal(word: answer) else {
+            wordError(title: "Word no recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
         withAnimation {
             usedWords.insert(answer, at: 0)
         }
         newWord = ""
+        score += 1
     }
     
     func startGame() {
+        score = 0
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL, encoding: .ascii) {
                 let allWords = startWords.components(separatedBy: "\n")
@@ -71,6 +112,19 @@ struct ContentView: View {
             }
         }
         return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showError = true
     }
 }
 
