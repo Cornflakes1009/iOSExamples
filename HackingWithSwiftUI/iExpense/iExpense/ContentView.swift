1190c1725de6ac67a08786abbd3cee39f82d4b94
Harold Davidson <harold.davidson89@gmail.com>
2025-07-06 09:27:25 -0700

    WIP

--- HackingWithSwiftUI/iExpense/iExpense/ContentView.swift
+++ HackingWithSwiftUI/iExpense/iExpense/ContentView.swift
@@ -5,9 +5,14 @@
 //  Created by Harold Davidson on 7/3/25.
 //
 
+// to get a UUID in console, you can type "uuidgen"
+
 import SwiftUI
 
-struct ExpenseItem {
+// this data type can be uniquely identified.
+// this makes it so we can delete the id part in ForEach(expenses.items, id: \.id) { item in
+struct ExpenseItem: Identifiable {
+    let id = UUID()
     let name: String
     let type: String
     let amount: Double
@@ -17,13 +22,14 @@ struct ExpenseItem {
 class Expenses {
     var items = [ExpenseItem]()
 }
+
 struct ContentView: View {
     @State private var expenses = Expenses()
     
     var body: some View {
         NavigationStack {
             List {
-                ForEach(expenses.items, id: \.name) { item in
+                ForEach(expenses.items) { item in
                     Text(item.name)
                 }
                 .onDelete(perform: removeItems)
