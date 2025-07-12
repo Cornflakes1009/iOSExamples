af5e1b6cb443d2a1232a92ce8de337ac4314ea50
Harold Davidson <harold.davidson89@gmail.com>
2025-07-11 23:52:46 -0700

    Create Moonshot project

---
+++ Projects/Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Harold Davidson on 7/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
