0f39ebb12f80a3c90b1ec3641be5aed189c4f40a
Harold Davidson <harold.davidson89@gmail.com>
2025-07-15 23:59:17 -0700

    WIP

--- Projects/Moonshot/Moonshot/ContentView.swift
+++ Projects/Moonshot/Moonshot/ContentView.swift
@@ -24,24 +24,47 @@ struct CustomText: View {
 
 struct ContentView: View {
     var body: some View {
-        //        Image(.example) // this is done from Xcode/Swift. The image name is actually "Example.jpg"
-        //            .resizable()
-        //            .scaledToFit()
-        //            //.frame(width: 300, height: 300)
-        //        // setting the frame for the image to be presented in 80% of the screen width (.horizonal).
-        //            .containerRelativeFrame(.horizontal) { size, axis in
-        //                size * 0.8
-        //            }
-        
-        ScrollView(.vertical) {
-            // LazyVStack only builds items as needed
-            LazyVStack(spacing: 10) {
-                ForEach(0..<100) {
-                    CustomText(text: "Item \($0)")
-                        .font(.headline)
+        NavigationStack {
+            
+            
+            //        Image(.example) // this is done from Xcode/Swift. The image name is actually "Example.jpg"
+            //            .resizable()
+            //            .scaledToFit()
+            //            //.frame(width: 300, height: 300)
+            //        // setting the frame for the image to be presented in 80% of the screen width (.horizonal).
+            //            .containerRelativeFrame(.horizontal) { size, axis in
+            //                size * 0.8
+            //            }
+            
+//            ScrollView(.vertical) {
+                List(0..<100) { row in
+                    NavigationLink("Row \(row)") {
+                        Text("Details for Row \(row)")
+                    }
                 }
-            }
-            .frame(maxWidth: .infinity) // if you don't set the width, it assumes that it's the width of the content and you can only scroll when you interact with the items themselves. It would be very thin.
+                .navigationTitle("SwiftUI")
+                // LazyVStack only builds items as needed
+//                LazyVStack(spacing: 10) {
+//                    ForEach(0..<100) {
+//                        CustomText(text: "Item \($0)")
+//                            .font(.headline)
+//                    }
+//                }
+//                .frame(maxWidth: .infinity) // if you don't set the width, it assumes that it's the width of the content and you can only scroll when you interact with the items themselves. It would be very thin.
+//            }
+//            Spacer()
+//            NavigationLink {
+//                Text("Detail View")
+//            } label: {
+//                VStack { // this whole view is the link
+//                    Text("This is the label")
+//                    Text("So is this")
+//                    Image(systemName: "face.smiling")
+//                }
+//            }
+//            .navigationTitle("SwiftUI")
+//            
+//            Spacer()
         }
     }
 }
