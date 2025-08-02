cbe5705c4ca270601a0bb24e1ae9a501405357ac
Harold Davidson <harold.davidson89@gmail.com>
2025-08-01 23:44:20 -0700

    WIP

--- Projects/Moonshot-Project/Moonshot-Project/ContentView.swift
+++ Projects/Moonshot-Project/Moonshot-Project/ContentView.swift
@@ -11,6 +11,10 @@ struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let misions: [Mission] = Bundle.main.decode("missions.json")
     
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     var body: some View {
         Text(String(astronauts.count))
     }
