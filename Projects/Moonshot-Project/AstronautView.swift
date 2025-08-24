4745ee0b1e435c977c63ef37d0f91893cb88dd4f
Harold Davidson <harold.davidson89@gmail.com>
2025-08-23 23:22:19 -0700

    Complete Moonshot

---
+++ Projects/Moonshot-Project/AstronautView.swift
@@ -0,0 +1,34 @@
+//
+//  AstronautView.swift
+//  Moonshot-Project
+//
+//  Created by Harold Davidson on 8/23/25.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    let astronaut: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    AstronautView(astronaut: astronaut["aldrin"]!)
+        .preferredColorScheme(.dark)
+}
