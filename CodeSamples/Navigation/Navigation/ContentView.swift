0fba3835329288252efe5c0780835c4118cba6cd
Harold Davidson <harold.davidson89@gmail.com>
2025-08-30 23:58:03 -0700

    WIP

--- CodeSamples/Navigation/Navigation/ContentView.swift
+++ CodeSamples/Navigation/Navigation/ContentView.swift
@@ -21,15 +21,43 @@ struct DetailView: View {
 }
 
 struct ContentView: View {
+    
+    @State private var path = [Int]()
+    
     var body: some View {
-        NavigationStack {
-            // problem with this approach is that it instantiates the view before it's needed
-//            NavigationLink("Tap Me") {
-//                DetailView(number: 556)
+//        NavigationStack {
+//            // problem with this approach is that it instantiates the view before it's needed
+////            NavigationLink("Tap Me") {
+////                DetailView(number: 556)
+////            }
+//            
+//            // this only happens when you choose an option. Much more efficient
+//            List(0..<100) { i in
+//                NavigationLink("Select \(i)", value: i)
 //            }
-            
-            List(0..<100) { i in
-                NavigationLink("Select \(i)", value: i)
+//            .navigationDestination(for: Int.self) { selection in
+//                Text("You selected \(selection)")
+//            }
+//        }
+        
+        // This option allows you to navigate once some other action is perform. Say once data has been fetched from an API."path" is a state variable.
+        NavigationStack(path: $path) {
+            VStack {
+                Button("Show 32") {
+                    path = [32]
+                }
+                
+                Button("Show 64") {
+                    path.append(64)
+                }
+                
+                // this will navigate twice
+                Button("Show 32 then 64") {
+                    path = [32, 64]
+                }
+            }
+            .navigationDestination(for: Int.self) { selection in
+                Text("You selected \(selection)")
             }
         }
     }
