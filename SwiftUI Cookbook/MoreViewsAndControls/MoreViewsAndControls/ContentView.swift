b47186156f8b65239fdca5c8f99a855c6a9d331b
Harold Davidson <harold.davidson89@gmail.com>
2025-10-09 23:47:52 -0700

    WIP

--- SwiftUI Cookbook/MoreViewsAndControls/MoreViewsAndControls/ContentView.swift
+++ SwiftUI Cookbook/MoreViewsAndControls/MoreViewsAndControls/ContentView.swift
@@ -14,13 +14,50 @@ struct ContentView: View {
     @State private var someText = "Initial Text"
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List {
+            Section(header: Text("ProgressViews")) {
+                ProgressView("Indertiminate progress view")
+                ProgressView("Downloading", value: progress, total: 2)
+                Button("More") {
+                    if progress < 2 {
+                        progress += 0.5
+                    }
+                }
+            }
+            
+            Section(header: Text("Labels")) {
+                Label("Slow", systemImage: "tortoise.fill")
+                Label {
+                    Text("Fast")
+                        .font(.title)
+                } icon : {
+                    Circle()
+                        .fill(Color.orange)
+                        .frame(width: 40, height: 20, alignment: .center)
+                        .overlay(Text("F"))
+                }
+            }
+            
+            Section(header: Text("ColorPicker")) {
+                ColorPicker(selection: $color) {
+                    Text("pick my background")
+                        .background(color)
+                        .padding()
+                }
+                ColorPicker("Picker", selection: $secondColor)
+            }
+            
+            Section(header: Text("Link")) {
+                Link("Packt Publishing", destination: URL(string: "https://www.packtpub.com/")!)
+            }
+            
+            Section(header: Text("TextEditor")) {
+                TextEditor(text: $someText)
+                Text("current editor text: \n\(someText)")
+            }
         }
-        .padding()
+        
+        //stopped on page 46 step 7
     }
 }
 
