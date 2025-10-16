dd2ddfb597ae740ca298f49da7b1e73f5327b6d9
Harold Davidson <harold.davidson89@gmail.com>
2025-10-15 23:52:24 -0700

    WIP

---
+++ Projects/GraphQLDemo/GraphQLDemo/GraphQLDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQLDemoApp.swift
+//  GraphQLDemo
+//
+//  Created by Harold Davidson on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct GraphQLDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
