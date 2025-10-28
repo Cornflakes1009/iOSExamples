f2fa9e6220b501cc51cd3aa60311acb8474ab18e
Harold Davidson <harold.davidson89@gmail.com>
2025-10-27 23:27:57 -0700

    WIP

--- CodeSamples/TDD - Udemy/Section 3/TipCalculator/TipCalculator/ContentView.swift
+++ CodeSamples/TDD - Udemy/Section 3/TipCalculator/TipCalculator/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
     @State private var tip: String?
     @State private var message: String = ""
     
+    let tipCalculator = TipCalculator()
+    
     var body: some View {
         NavigationView {
             
@@ -32,7 +34,27 @@ struct ContentView: View {
 
                 
                 Button("Calculate Tip") {
-                  
+                    message = ""
+                    tip = ""
+                    
+                    guard let total = Double(total) else {
+                        message = "Invalid Input"
+                        return
+                    }
+                    
+                    do {
+                        let result = try tipCalculator.calculate(total: total, tipPercentage: tipPercentage)
+                        
+                        let formatter = NumberFormatter()
+                        formatter.numberStyle = .currency
+                        self.tip = formatter.string(from: NSNumber(value: result))
+                        
+                    } catch TipCalculatorError.invalidInput {
+                        message = "Invalid Input"
+                    } catch {
+                        message = error.localizedDescription
+                    }
+                    
                     
                 }.padding(.top, 20)
                 
