e7b866cb23ca3d317a919e631a6cf3896cbb75ab
Harold Davidson <harold.davidson89@gmail.com>
2025-11-01 23:51:51 -0700

    refactor and add setUp and cleanUp methods to UI tests

--- CodeSamples/TDD - Udemy/Section 3/TipCalculator/TipCalculatorUITests/TipCalculatorUITestsLaunchTests.swift
+++ CodeSamples/TDD - Udemy/Section 3/TipCalculator/TipCalculatorUITests/TipCalculatorUITestsLaunchTests.swift
@@ -9,24 +9,33 @@ import XCTest
 
 final class when_content_view_is_shown: XCTestCase {
 
-    func test_should_make_sure_that_the_total_text_field_contains_default_value() {
-        let app = XCUIApplication()
+    private var app: XCUIApplication!
+    
+    // this runs before every test case. Not just once.
+    override func setUp() { // make sure you don't use the CLASS version
+        app = XCUIApplication()
         continueAfterFailure = false
         app.launch()
+    }
+    
+    func test_should_make_sure_that_the_total_text_field_contains_default_value() {
+        
         
         let totalTextField = app.textFields["totalTextField"] // set in ContentView in accessibilityIdentifier
         XCTAssertEqual(totalTextField.value as? String, "Enter total")
     }
     
     func test_should_make_sure_the_20_percent_default_tip_option_is_selected() {
-        let app = XCUIApplication()
-        continueAfterFailure = false
-        app.launch()
         
         let tipPercentageSegmentedControl = app.segmentedControls["tipPercentageSegmentedControl"] // set in ContentView in accessibilityIdentifier
         let segmentedControlButton = tipPercentageSegmentedControl.buttons.element(boundBy: 1)
         
         XCTAssertEqual(segmentedControlButton.label, "20%")
         XCTAssertTrue(segmentedControlButton.isSelected)
+    }
+    
+    // runs after each test has been ran
+    override func tearDown() {
+        // clean up
     }
 }
