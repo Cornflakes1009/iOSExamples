31421c590308014b474c529b0ebc6d4da3dca54c
Harold Davidson <harold.davidson89@gmail.com>
2025-11-28 23:52:48 -0800

    Complete Page OBject for mocking section

---
+++ CodeSamples/TDD - Udemy/Section 6/MockingDemo/MockingDemoUserInterfaceTests/Page Objects/LoginPageObject.swift
@@ -0,0 +1,33 @@
+//
+//  LoginPageObject.swift
+//  MockingDemo
+//
+//  Created by Harold Davidson on 11/28/25.
+//
+
+import XCTest
+
+class LoginPageObject {
+    
+    private var app: XCUIApplication
+    
+    init(app: XCUIApplication) {
+        self.app = app
+    }
+    
+    var usernameTextField: XCUIElement {
+        app.textFields["usernameTextField"]
+    }
+    
+    var passwordTextField: XCUIElement {
+        app.textFields["passwordTextField"]
+    }
+    
+    var loginButton: XCUIElement {
+        app.buttons["loginButton"]
+    }
+    
+    var messageText: XCUIElement {
+        app.staticTexts["messageText"]
+    }
+}
